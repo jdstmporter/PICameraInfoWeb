45c99450ffa8516596d864ea1190c62db6d2718d
julianporter <julian@porternet.net>
2024-09-21 20:12:39 +0100

    Restructure WSGI

--- src/actions/wsgi/app.py
+++ src/actions/wsgi/app.py
@@ -1,8 +1,10 @@
 import re
-from http import HTTPStatus
+from http import HTTPStatus, HTTPMethod
+from collections import defaultdict
 from util import Logger
 
 from data import DataStore
+
 
 
 
@@ -35,7 +37,10 @@ class WSGIEnv:
         return self._env[key]
 
     def method(self):
-        return self._env['REQUEST_METHOD']
+        m = self._env['REQUEST_METHOD']
+        return HTTPMethod.__members__[m]
+
+
 
     def path(self):
         p = self['PATH_INFO']
@@ -47,8 +52,8 @@ class WSGIEnv:
             try:
                 key = re.match('^HTTP_(\S+)$',k).groups()[0]
                 out[key]=v
-            except:
-                pass
+            except Exception as e:
+                Logger.log.error(f'Error getting WSGI headers : {e}')
         return out
 
     def query(self):
@@ -59,60 +64,47 @@ class WSGIEnv:
                 try:
                     k, v = re.match('^([^=]+)=(.*)$',q).groups()
                     out[k]=v
-                except:
-                    pass
+                except Exception as e:
+                    Logger.log.error(f'Error processing WSGI query parameter {q} : {e}')
             return out
-        except:
+        except Exception as e:
+            Logger.log.error(f'Error getting WSGI query parameters : {e}')
             return dict()
 
+class WSGIApp:
 
-
-
-
-
-
-
-class BaseWSGIApp:
-    def __init__(self):
-        pass
-
-    def GET(self,env):
-        return ResponseObject()
-
-    def POST(self,env):
-        return ResponseObject()
-
-    def DELETE(self,env):
-        return ResponseObject()
-
-    def act(self,env):
-        try:
-            method = env.method()
-            action = getattr(self, method)
-            return action(env)
-        except Exception as e:
-            return ErrorObject(status=HTTPStatus.BAD_REQUEST,
-                               text=str(e))
-
-    def __call__(self, environ, start_response):
-        env = WSGIEnv(environ)
-        responder = self.act(env)
-        responder(start_response)
-        return responder.text
-
-class WSGIApp(BaseWSGIApp):
+    @classmethod
+    def asCSV(cls,info):
+        lines = []
+        lines.append('timestamp,voltage,current,percentage')
+        for row in info:
+            row_str = [str(i) for i in row]
+            lines.append(f"'{row_str[0]}',{row_str[1]},{row_str[2]},{row_str[3]}")
+        return '\n'.join(lines)
 
     def __init__(self, json):
-        super().__init__()
         self.json = json
         self.length = len(json)
         self.datastore = DataStore()
 
+    def _responder(self, env):
+        try:
+            if env.method() == HTTPMethod.GET:
+                return self.GET(env)
+            else:
+                return ErrorObject(status=HTTPStatus.BAD_REQUEST)
+        except Exception as e:
+            return ErrorObject(status=HTTPStatus.INTERNAL_SERVER_ERROR, text=str(e))
+
+    def __call__(self, environ, start_response):
+        env = WSGIEnv(environ)
+        responder = self._responder(env)
+        responder(start_response)
+        return responder.text
 
     def GET(self,env):
         try:
             path = env.path()
-
             if len(path) == 0:
                 pass
             elif path == 'cams':
@@ -121,12 +113,7 @@ class WSGIApp(BaseWSGIApp):
                 params = env.query()
                 if 'since' in params:
                     info = self.datastore.all_battery(params['since'])
-                    lines = []
-                    lines.append('timestamp,voltage,current,percentage')
-                    for row in info:
-                        row_str = [str(i) for i in row]
-                        lines.append(f"'{row_str[0]}',{row_str[1]},{row_str[2]},{row_str[3]}")
-                    return ResponseObject(content_type='text/csv',text='\n'.join(lines))
+                    return ResponseObject(content_type='text/csv',text=self.asCSV(info))
                 else:
                     info = self.datastore.battery_json()
                     return DataObject(info)
@@ -137,6 +124,8 @@ class WSGIApp(BaseWSGIApp):
             Logger.log.error(f'Error: {e}')
             return ErrorObject(status=HTTPStatus.INTERNAL_SERVER_ERROR,
                                   text=str(e))
+
+
 
 
 
